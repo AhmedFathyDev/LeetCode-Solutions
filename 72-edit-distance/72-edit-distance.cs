16e3aa69890fba0e2c987de62dc0c26961968de9
Ahmed Fathy <ahmed.fathy.dev@gmail.com>
2022-06-15 03:51:46 +0200

    Time: 90 ms (58.02%), Space: 37 MB (61.77%) - LeetHub

--- 72-edit-distance/72-edit-distance.cs
+++ 72-edit-distance/72-edit-distance.cs
@@ -2,26 +2,26 @@ public class Solution
 {
     public int MinDistance(string word1, string word2)
     {
-        int[,] db = new int[word2.Length + 1, word1.Length + 1];
+        int[,] DPTable = new int[word2.Length + 1, word1.Length + 1];
 
         for (int j = 1; j <= word1.Length; ++j)
         {
-            db[0, j] = j;
+            DPTable[0, j] = j;
         }
 
         for (int i = 1; i <= word2.Length; ++i)
         {
-            db[i, 0] = i;
+            DPTable[i, 0] = i;
         }
 
         for (int i = 1; i <= word2.Length; ++i)
         {
             for (int j = 1; j <= word1.Length; ++j)
             {
-                db[i, j] = Math.Min(db[i - 1, j] + 1, Math.Min(db[i, j - 1] + 1, db[i - 1, j - 1] + (word1[j - 1] == word2[i - 1] ? 0 : 1)));
+                DPTable[i, j] = Math.Min(DPTable[i - 1, j] + 1, Math.Min(DPTable[i, j - 1] + 1, DPTable[i - 1, j - 1] + (word1[j - 1] == word2[i - 1] ? 0 : 1)));
             }
         }
 
-        return db[word2.Length, word1.Length];
+        return DPTable[word2.Length, word1.Length];
     }
 }
