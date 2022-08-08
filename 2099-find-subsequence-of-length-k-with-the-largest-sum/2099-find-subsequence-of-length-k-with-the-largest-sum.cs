df968df5046d0cd970a0cc16cc6a39fca0e2c8d1
Ahmed Fathy <ahmed.fathy.dev@gmail.com>
2022-08-08 04:48:21 +0200

    Time: 176 ms (74.24%), Space: 44.2 MB (12.12%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cs
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cs
@@ -1,25 +1,23 @@
 public class Solution {
     public int[] MaxSubsequence(int[] nums, int k) {
-                var sortedNums = new int[nums.Length];
+        var sortedNums = new int[nums.Length];
+        
         Array.Copy(nums, 0, sortedNums, 0, nums.Length);
         Array.Sort(sortedNums);
-        Array.Reverse(sortedNums);
 
         var maxNums = new List<int>();
         var subNums = new List<int>();
         
-        for (var i = 0; i < k; ++i)
+        for (var i = sortedNums.Length - k; i < sortedNums.Length; ++i)
         {
             maxNums.Add(sortedNums[i]);
         }
 
         foreach (var num in nums)
         {
-            if (maxNums.Any(maxNum => num == maxNum))
-            {
-                subNums.Add(num);
-                maxNums.Remove(num);
-            }
+            if (maxNums.All(maxNum => num != maxNum)) continue;
+            subNums.Add(num);
+            maxNums.Remove(num);
         }
 
         return subNums.ToArray();
