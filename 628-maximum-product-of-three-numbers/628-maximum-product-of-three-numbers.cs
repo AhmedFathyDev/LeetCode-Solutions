ca2cad38a8297501c546c516eaf763a5caa849ef
Ahmed Fathy <ahmed.fathy.dev@gmail.com>
2022-08-09 20:45:36 +0200

    Time: 175 ms (57.69%), Space: 41.6 MB (31.73%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cs
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cs
@@ -1,7 +1,6 @@
 public class Solution {
     public int MaximumProduct(int[] nums) {
         Array.Sort(nums);
-
         return Math.Max(nums[^1] * nums[^2] * nums[^3], nums[0] * nums[1] * nums[^1]);
     }
 }
