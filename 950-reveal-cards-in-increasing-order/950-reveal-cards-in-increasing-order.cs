7e20ffc1dd3254022a1d77435f8ae47922c2a6e2
Ahmed Fathy <ahmed.fathy.dev@gmail.com>
2022-08-10 18:35:13 +0200

    Time: 247 ms (24.00%), Space: 41.8 MB (96.00%) - LeetHub

---
+++ 950-reveal-cards-in-increasing-order/950-reveal-cards-in-increasing-order.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] DeckRevealedIncreasing(int[] deck) {
+        var answerOrder = new int[deck.Length];
+        var indices = new Queue<int>();
+
+        for (var i = 0; i < deck.Length; ++i)
+        {
+            indices.Enqueue(i);
+        }
+
+        Array.Sort(deck);
+
+        foreach (var card in deck)
+        {
+            answerOrder[indices.Dequeue()] = card;
+
+            if (indices.Count <= 0) continue;
+            
+            indices.Enqueue(indices.Dequeue());
+        }
+
+        return answerOrder;
+    }
+}
