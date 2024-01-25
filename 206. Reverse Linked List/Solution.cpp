02ef5ff51aebd07666370154b3b929ab77e5722b
Ahmed Osman <SWE.Ahmed.Osman@gmail.com>
2024-01-25 10:37:26 +0200

    solve: 206. Reverse Linked List problem

---
+++ 206. Reverse Linked List/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+
+        ListNode *temp_list = reverseList(head->next);
+
+        head->next->next = head;
+        head->next = nullptr;
+
+        return temp_list;
+    }
+};
