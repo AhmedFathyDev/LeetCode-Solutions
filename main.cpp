1f08b061e9e91ed040b30c712b6085365f149138
Ahmed Osman <SWE.Ahmed.Osman@gmail.com>
2024-02-03 21:04:32 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define PI acos(-1)
+#define MAX(X, Y) ((X) < (Y) ? (Y) : (X))
+#define MIN(X, Y) ((X) > (Y) ? (Y) : (X))
+#define CEIL(X, Y) (((X) + (Y)-1) / (Y))
+#define NUMBER_OF_DIGITS(X) floor(1 + log10(X))
+#define TOGGLE_CASE(CHARACTER) ((CHARACTER) ^ (1 << 5))
+#define WATCH(X) std::cout << (#X) << " = " << X << endl
+#define SWAP(X, Y) ((X) ^= (Y)), ((Y) ^= (X)), ((X) ^= (Y))
+#define SET_BIT(VARIABLE, INDEX) ((VARIABLE) | 1 << (INDEX))
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return EXIT_SUCCESS;
+}
