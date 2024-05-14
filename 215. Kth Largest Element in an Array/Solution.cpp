e41249fe479681096f4bf30e504cd92f7b568473
Ahmed Fathy <SWE.Ahmed.Osman@gmail.com>
2024-05-14 14:48:17 +0300

    solve: 215. Kth Largest Element in an Array problem

---
+++ 215. Kth Largest Element in an Array/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int findKthLargest(vector<int> &nums, int k)
+    {
+        priority_queue<int> pq;
+
+        for (int num : nums)
+            pq.push(num);
+
+        while (--k)
+            pq.pop();
+
+        return pq.top();
+    }
+};
