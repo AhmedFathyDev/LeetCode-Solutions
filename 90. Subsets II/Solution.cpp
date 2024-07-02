d82a246318056f9f6094b91f3e5ff9556bcf9f1b
Ahmed Fathy <SWE.Ahmed.Osman@gmail.com>
2024-07-02 19:59:45 +0300

    solve: 90. Subsets II problem

---
+++ 90. Subsets II/Solution.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+private:
+    void backTrack(vector<vector<int>> &possibleSubsets, vector<int> newSubsets, vector<int> &nums, int start)
+    {
+        possibleSubsets.push_back(newSubsets);
+
+        for (int i = start; i < nums.size(); ++i)
+        {
+            if (i > start && nums[i] == nums[i - 1])
+                continue; // skip duplicates
+
+            newSubsets.push_back(nums[i]);
+            backTrack(possibleSubsets, newSubsets, nums, i + 1);
+            newSubsets.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> subsetsWithDup(vector<int> &nums)
+    {
+        vector<vector<int>> possibleSubsets;
+        sort(nums.begin(), nums.end());
+        backTrack(possibleSubsets, {}, nums, 0);
+        return possibleSubsets;
+    }
+};
