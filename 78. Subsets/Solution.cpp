bac39bdd610a6285e57b2a3031aef2dd52077a2d
Ahmed Fathy <SWE.Ahmed.Osman@gmail.com>
2024-07-02 20:41:55 +0300

    update: 78. Subsets solution

--- 78. Subsets/Solution.cpp
+++ 78. Subsets/Solution.cpp
@@ -1,14 +1,14 @@
 class Solution
 {
 private:
-    void backTrack(vector<vector<int>> &possibleSubsets, vector<int> newSubsets, vector<int> &nums, int start)
+    void backtrack(vector<vector<int>> &possibleSubsets, vector<int> newSubsets, vector<int> &nums, int start)
     {
         possibleSubsets.push_back(newSubsets);
 
         for (int i = start; i < nums.size(); ++i)
         {
             newSubsets.push_back(nums[i]);
-            backTrack(possibleSubsets, newSubsets, nums, i + 1);
+            backtrack(possibleSubsets, newSubsets, nums, i + 1);
             newSubsets.pop_back();
         }
     }
@@ -17,7 +17,7 @@ public:
     vector<vector<int>> subsets(vector<int> &nums)
     {
         vector<vector<int>> possibleSubsets;
-        backTrack(possibleSubsets, {}, nums, 0);
+        backtrack(possibleSubsets, {}, nums, 0);
         return possibleSubsets;
     }
 };
