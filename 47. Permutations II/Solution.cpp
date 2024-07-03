e0ff6ce0219df6f349ebd9b5b2d13e0593e40f8a
Ahmed Fathy <SWE.Ahmed.Osman@gmail.com>
2024-07-03 12:14:09 +0300

    update: 47. Permutations II solution

--- 47. Permutations II/Solution.cpp
+++ 47. Permutations II/Solution.cpp
@@ -8,7 +8,7 @@ private:
         else
             for (int i = 0; i < nums.size(); ++i)
             {
-                if (used[i] || i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
+                if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]))
                     continue;
 
                 used[i] = true;
