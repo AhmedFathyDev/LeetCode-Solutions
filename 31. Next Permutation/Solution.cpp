e6890b9ad8478fa1d6c07db0ba969724b86d06ac
Ahmed Fathy <SWE.Ahmed.Osman@gmail.com>
2024-07-04 15:14:13 +0300

    solve: 31. Next Permutation problem

---
+++ 31. Next Permutation/Solution.cpp
@@ -0,0 +1,25 @@
+#define SWAP(X, Y) ((X) ^= (Y)), ((Y) ^= (X)), ((X) ^= (Y))
+
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &nums)
+    {
+        int i = nums.size() - 2;
+
+        while (i >= 0 && nums[i + 1] <= nums[i])
+            i--;
+
+        if (i >= 0)
+        {
+            int j = nums.size() - 1;
+
+            while (nums[j] <= nums[i])
+                j--;
+
+            SWAP(nums[i], nums[j]);
+        }
+
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
